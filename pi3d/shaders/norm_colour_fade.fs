b7d4ccb577ab170af33b5c72f9599200b9bc52d1
crashmatt <uavflightdirector@gmail.com>
2015-09-11 01:06:19 +0200

    Add shader

---
+++ pi3d/shaders/norm_colour_fade.fs
@@ -0,0 +1,12 @@
+#include std_head_fs.inc
+
+varying float dist;
+varying vec3 colour;
+varying float alpha;
+
+void main(void) {
+#include std_main_mat.inc
+  //if (distance(gl_PointCoord, vec2(0.5)) > 0.5) discard; //circular points
+  gl_FragColor = vec4(colour, 1.0);  //(1.0 - ffact) * texc + ffact * vec4(colour, unif[5][1]); // ------ combine using factors
+  gl_FragColor.a *= unif[5][2] * alpha;
+}
